996a734252bf8e400abe0c5842f0516e23f632c2
Juan Robledo <heaxenz@Rikos-PC>
2023-04-29 18:46:50 -0700

    repo start

---
+++ app.js
@@ -0,0 +1,20 @@
+const con = document.querySelector('.letters');
+
+con.style.color = 'red'
+
+console.log('hello')
+
+// function randomRGB(){
+//   const r = Math.floor(Math.random() * 256);
+//   const g = 0;
+//   const b = Math.floor(Math.random() * 256);
+//   return `rgb(${r},${g},${b})`
+
+// }
+
+// const letters = document.querySelector('.letters');
+// const intervalId = setInterval(function () {
+//   for(let letter of letters) {
+//     letter.style.color = randomRGB();
+//   }
+// }, 1000);
